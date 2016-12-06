16688fe18ce294d1d04199dee15db5a9b7b42261
negiyk <kumar.yashpal@gmail.com>
2016-12-06 12:15:29 +0000

    Setup Project in GIT

--- Downloads/Java-Coding-Test/JavaTest/CheckoutSystem/src/main/java/hmrc/CheckOutSystem.java
+++ Downloads/Java-Coding-Test/JavaTest/CheckoutSystem/src/main/java/hmrc/CheckOutSystem.java
@@ -23,12 +23,45 @@ public class CheckOutSystem {
             return totalCost;
         }
 
-        totalCost = cart
+/*        totalCost = cart
                 .stream()
                 .mapToDouble(Item::getItemPrice)
-                .sum();
+                .sum();*/
+
+        totalCost = applyDiscount();
 
         return totalCost;
+    }
+
+    private double applyDiscount() {
+
+        long amountOfApple = getItemCount(Fruit.Apple.name());
+
+        long amountOfOrange =  getItemCount(Fruit.Orange.name());
+
+        if ( amountOfOrange > 0  )
+            totalCost  = apply3For2Discount(amountOfOrange);
+
+
+        if ( amountOfApple > 0  )
+            totalCost = totalCost+ apply2For1Discount(amountOfApple);
+
+
+        return totalCost;
+
+    }
+
+    private long getItemCount(String itemNameToCount) {
+        return cart.stream().filter(item -> itemNameToCount.equals(item.getItemName())).count();
+    }
+
+    private double apply3For2Discount(long amountOfOrange) {
+        return (amountOfOrange /3)* 2 * Fruit.Orange.getPrice()  +(amountOfOrange % 3) * Fruit.Orange.getPrice();
+
+    }
+
+    private double apply2For1Discount(long amountOfApple) {
+        return (amountOfApple /2) * Fruit.Apple.getPrice() + (amountOfApple % 2) * Fruit.Apple.getPrice();
     }
 
     @Override
--- Downloads/Java-Coding-Test/JavaTest/CheckoutSystem/src/test/java/hmrc/CheckOutSystemTest.java
+++ Downloads/Java-Coding-Test/JavaTest/CheckoutSystem/src/test/java/hmrc/CheckOutSystemTest.java
@@ -66,15 +66,55 @@ public class CheckOutSystemTest {
         assertEquals(2.05, checkOutSystem.totalCost(), 0);
     }
 
+    @Test
+    public void calcTotalAfterApplyAppleDiscountBuyOneGetOneFreeOnTwoApples(){
+        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
+        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
+        assertEquals(.60, checkOutSystem.totalCost(), 0);
 
+    }
 
+    @Test
+    public void calcTotalAfterApplyAppleDiscountBuyOneGetOneFreeOnThreeApples(){
+        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
+        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
+        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
+        assertEquals(1.20, checkOutSystem.totalCost(), 0.0);
+
+    }
+
+    @Test
+    public void calcTotalAfterApplyOrangeDiscountThreeForTwoPriceOnThreeOranges(){
+        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        assertEquals(0.50, checkOutSystem.totalCost(), 0);
+    }
+
+    @Test
+    public void calcTotalAfterApplyOrangeDiscountThreeForTwoPriceOnFourOranges(){
+        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        assertEquals(0.75, checkOutSystem.totalCost(), 0);
+
+    }
+
+    @Test
+    public void calcTotalAfterApplyOrangeDiscountThreeForTwoPriceOnFiveOranges(){
+        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        assertEquals(1.00, checkOutSystem.totalCost(), 0);
+
+    }
     @After
     public void printItems(){
 
         System.out.println(checkOutSystem.toString());
     }
-
-
-
 
 }
