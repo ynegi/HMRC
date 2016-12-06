365a234c62b9376eca5aec1576f8fe44f16a0b10
negiyk <kumar.yashpal@gmail.com>
2016-12-06 22:11:43 +0000

    Setup Project in GIT

--- Downloads/Java-Coding-Test/JavaTest/CheckoutSystem/src/test/java/hmrc/CheckOutSystemTest.java
+++ Downloads/Java-Coding-Test/JavaTest/CheckoutSystem/src/test/java/hmrc/CheckOutSystemTest.java
@@ -25,89 +25,105 @@ public class CheckOutSystemTest {
 
     @Test
     public void calcTotalCostWhenCartHasOneApple(){
-        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
+        checkOutSystem.addItem(getApple(), getApplePrice());
         assertEquals(0.60, checkOutSystem.totalCost(), 0);
+    }
+
+    private double getApplePrice() {
+        return Fruit.Apple.getPrice();
     }
 
     @Test
     public void calcTotalCostWhenCartHasOneOrange(){
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
         assertEquals(0.25, checkOutSystem.totalCost(), 0);
     }
 
     @Test
     public void calcTotalCostWhenCartHas2Apples(){
-        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
-        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
+        checkOutSystem.addItem(getApple(), getApplePrice());
+        checkOutSystem.addItem(getApple(), getApplePrice());
         assertEquals(1.20, checkOutSystem.totalCost(), 0);
     }
 
     @Test
     public void calcTotalCostWhenCartHas2Orange(){
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
 
         assertEquals(0.5, checkOutSystem.totalCost(), 0);
 
     }
     @Test
     public void calcTotalCostWhenCartHasOneAppleOneOrange(){
-        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        checkOutSystem.addItem(getApple(), getApplePrice());
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
         assertEquals(0.85, checkOutSystem.totalCost(), 0);
     }
 
     @Test
     public void calcTotalCostWhenCartHasAppleAppleOrangeApple(){
-        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
-        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
-        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
+        checkOutSystem.addItem(getApple(), getApplePrice());
+        checkOutSystem.addItem(getApple(), getApplePrice());
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
+        checkOutSystem.addItem(getApple(), getApplePrice());
         assertEquals(2.05, checkOutSystem.totalCost(), 0);
+    }
+
+    private String getOrange() {
+        return Fruit.Orange.name();
     }
 
     @Test
     public void calcTotalAfterApplyAppleDiscountBuyOneGetOneFreeOnTwoApples(){
-        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
-        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
+        checkOutSystem.addItem(getApple(), getApplePrice());
+        checkOutSystem.addItem(getApple(), getApplePrice());
         assertEquals(.60, checkOutSystem.totalCost(), 0);
 
     }
 
+    private String getApple() {
+        return Fruit.Apple.name();
+    }
+
     @Test
     public void calcTotalAfterApplyAppleDiscountBuyOneGetOneFreeOnThreeApples(){
-        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
-        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
-        checkOutSystem.addItem(Fruit.Apple.name(), 0.60);
+        checkOutSystem.addItem(getApple(), getApplePrice());
+        checkOutSystem.addItem(getApple(), getApplePrice());
+        checkOutSystem.addItem(getApple(), getApplePrice());
         assertEquals(1.20, checkOutSystem.totalCost(), 0.0);
 
     }
 
     @Test
     public void calcTotalAfterApplyOrangeDiscountThreeForTwoPriceOnThreeOranges(){
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
         assertEquals(0.50, checkOutSystem.totalCost(), 0);
+    }
+
+    private double getOrangePrice() {
+        return Fruit.Orange.getPrice();
     }
 
     @Test
     public void calcTotalAfterApplyOrangeDiscountThreeForTwoPriceOnFourOranges(){
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
         assertEquals(0.75, checkOutSystem.totalCost(), 0);
 
     }
 
     @Test
     public void calcTotalAfterApplyOrangeDiscountThreeForTwoPriceOnFiveOranges(){
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
-        checkOutSystem.addItem(Fruit.Orange.name(), 0.25);
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
+        checkOutSystem.addItem(getOrange(), getOrangePrice());
         assertEquals(1.00, checkOutSystem.totalCost(), 0);
 
     }
